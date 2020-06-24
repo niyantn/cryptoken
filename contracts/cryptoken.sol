b4586d99cf01aed801a2327b4a04e569f9baf8b9
Niyant Narang <niyant.narang@gmail.com>
2020-06-24 13:35:34 -0400

    3. delegated transfer

--- contracts/cryptoken.sol
+++ contracts/cryptoken.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.4.20;
 contract cryptoken{
     uint256 private _totalSupply;
     mapping (address => uint256) private _balances;
+    mapping (address => mapping(address => uint256)) private _allowed;
     string private _name = "Ming";
     string private _symbol = "MNG";
 
@@ -12,6 +13,12 @@ contract cryptoken{
         address indexed _to,
         uint256 _value
     );
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     constructor(uint256 supply) public {
         _totalSupply = supply;
         _balances[msg.sender] = supply;
@@ -29,10 +36,33 @@ contract cryptoken{
         return _symbol;
     }
     function transfer(address to, uint256 val) public returns (bool){
-        require(val<= _balances[msg.sender]);
+        require(val <= _balances[msg.sender]);
+        require(to != address(0));
+
         _balances[msg.sender] -= val;
         _balances[to] += val;
         emit Transfer(msg.sender, to, val);
+        return true;
+    }
+    function approve(address spender, uint256 val) public returns (bool){
+        require(spender != address(0));
+
+        _allowed[msg.sender][spender] = val;
+        emit Approval(msg.sender, spender, val);
+        return true;
+    }
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return _allowed[owner][spender];
+    }
+    function transferFrom(address from, address to, uint256 val) public returns (bool){
+        require(val <= _balances[from]);
+        require(val <= _allowed[from][msg.sender]);
+        require(to != address(0));
+
+        _balances[from] -= val;
+        _balances[to] += val;
+        _allowed[from][msg.sender] -= val;
+        emit Transfer(from, to, val);
         return true;
     }
 }
--- test/cryptoken.js
+++ test/cryptoken.js
@@ -53,4 +53,61 @@ contract('MyToken', function(accounts) {
             assert.equal(balance.toNumber(), 750000, 'deducts the amount from the sending account');
         });
     });
+
+    it('approves tokens for delegated transfer', function() {
+        return MyToken.deployed().then(function(instance) {
+            tokeninstance = instance;
+            return tokeninstance.approve.call(accounts[1], 100);
+        }).then(function(success) {
+            assert.equal(success, true, 'it returns true');
+            return tokeninstance.approve(accounts[1], 100, { from: accounts[0] });
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Approval', 'should be Approval event');
+            assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs account tokens are owned by');
+            assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs account tokens can be spent by');
+            assert.equal(receipt.logs[0].args._value, 100, 'logs approval amount');
+            return tokeninstance.allowance(accounts[0], accounts[1]);
+        }).then(function(allowance) {
+            assert.equal(allowance.toNumber(), 100, 'allocates allowance from owner to spender');
+        });
+    });
+
+    it('handles delegated token transfers', function() {
+        return MyToken.deployed().then(function(instance) {
+            tokeninstance = instance;
+            fromacc = accounts[2];
+            toacc = accounts[3];
+            spendacc = accounts[4];
+            return tokeninstance.transfer(fromacc, 100, { from: accounts[0] });
+        }).then(function(receipt) {
+            return tokeninstance.approve(spendacc, 10, { from: fromacc });
+        }).then(function(receipt) {
+            return tokeninstance.transferFrom(fromacc, toacc, 9999, { from: spendacc });
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'cannot transfer value larger than balance');
+            return tokeninstance.transferFrom(fromacc, toacc, 20, { from: spendacc });
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'cannot transfer value larger than allowed');
+            return tokeninstance.allowance(fromacc, spendacc);
+        }).then(function(allowance) {
+            assert.equal(allowance.toNumber(), 10, 'allowance allocated');
+            return tokeninstance.transferFrom(fromacc, toacc, 10, { from: spendacc });
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Transfer', 'should be Transfer event');
+            assert.equal(receipt.logs[0].args._from, fromacc, 'logs account tokens are transferred from');
+            assert.equal(receipt.logs[0].args._to, toacc, 'logs account tokens are trasnferred to');
+            assert.equal(receipt.logs[0].args._value, 10, 'logs transfer amount');
+            return tokeninstance.allowance(fromacc, spendacc);
+        }).then(function(allowance) {
+            assert.equal(allowance.toNumber(), 0, 'allowance should be spent');
+            return tokeninstance.balanceOf(fromacc);
+        }).then(function(bal) {
+            assert.equal(bal.toNumber(), 90, 'allowance should be taken from owner account');
+            return tokeninstance.balanceOf(toacc);
+        }).then(function(bal) {
+            assert.equal(bal.toNumber(), 10, 'allowance should be allocated to target account');
+        });
+    });
 })
